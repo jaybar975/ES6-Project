5b2664847f1fa54ea2d13c088f5c3c5e0bbfec7c
Justin Aybar <jaybar975@west-mec.org>
2019-09-09 15:26:47 -0700

    ES6 Project Commit #3
    Added a new footer to the website. Added a new background image and a new color for the navbar. Added some content to the website stating all about ES6

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
   });
-
-  // import { square } from './lib';
-  // document.getElementById('message').innerText = "The Square of 2 is " + square(2);
-  import { square } from './lib';
-  import $ from 'jquery';
---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function rendor() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+  export default App;
